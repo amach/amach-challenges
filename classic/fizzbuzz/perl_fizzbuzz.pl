6f9087cb2af143899408c47257d378c9b5c31af6
Anna Mach <anna.mach@gmail.com>
2012-07-03 02:21:09 -0400

    Perl implementation of FizzBuzz

---
+++ classic/fizzbuzz/perl_fizzbuzz.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use strict;
+
+foreach (1..100) {
+    if ($_ % 15 == 0) {
+        print "FizzBuzz\n";
+    } elsif ($_ % 3 == 0) {
+        print "Fizz\n";
+    } elsif ($_ % 5 == 0) {
+        print "Buzz\n";
+    } else {
+        print "$_\n";
+    }
+}
