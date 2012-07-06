ab3d0f7ddbad381fac233852bd3605f84e3c617f
Anna Mach <anna.mach@gmail.com>
2012-07-06 19:48:44 -0400

    Updated so that palindromes with punctuation and inconsistent spacing will be recognized as well -- e.g. "Madam I'm Adam" and "A man, a plan, a canal - Panama!" are now both recognized as palindromes.

--- classic/palindrome/python_palindrome.py
+++ classic/palindrome/python_palindrome.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
 def palin(st):
-  return st.lower() == st[::-1].lower()
+  
+  # remove non-alphanumeric chars from the string
+  alphanum_st = ''.join(c for c in st if c.isalnum())
+
+  # convert to lowercase
+  alphanum_st = alphanum_st.lower()
+
+  # compare the string to its reverse
+  return alphanum_st == alphanum_st[::-1]
 
 def main():
   
   st = raw_input('Enter your potential palindrome: ')
 
   if palin(st):
-    print st + ' is a palindrome!'
+    print '"' + st + '" is a palindrome!'
   else:
-    print st + ' is not a palindrome!'
+    print '"' + st + '" is not a palindrome!'
 
 if __name__ == '__main__':
   main()
